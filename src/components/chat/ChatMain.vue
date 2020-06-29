23b3a7f0d0a7804b4f6e505eb9700a1aab0ea1a0
lnatu <dfvutien@gmail.com>
2020-06-29 23:37:01 +0700

    app views - 40%

---
+++ src/components/chat/ChatMain.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="chats">
+    <div class="conversation">
+      <div class="conversation-header">
+        <div class="media">
+          <div class="media__avatar mr-2">
+            <img src="@/assets/img/users/girl.png" alt="girl" />
+          </div>
+          <div class="media__body">
+            <h6 class="media__name">Demi Lovato</h6>
+            <p class="media__status text-muted mt-sm">Online</p>
+          </div>
+        </div>
+        <ul class="media-nav list-style-none">
+          <li class="media-nav__item">
+            <a class="media-nav__link" href="#">
+              <svg class="media-nav__icon">
+                <use xlink:href="@/assets/img/icons/sprites.svg#icon-search" />
+              </svg>
+            </a>
+          </li>
+          <li class="media-nav__item">
+            <a class="media-nav__link" href="#">
+              <svg class="media-nav__icon">
+                <use xlink:href="@/assets/img/icons/sprites.svg#icon-phone-call" />
+              </svg>
+            </a>
+          </li>
+          <li class="media-nav__item">
+            <a class="media-nav__link" href="#">
+              <svg class="media-nav__icon">
+                <use xlink:href="@/assets/img/icons/sprites.svg#icon-more-vert" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ChatMain'
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/_conversation';
+</style>
