a11aa403d849efcef8829ea05d893630f6161fdf
dfvutien <dfvutien@gmail.com>
2020-07-15 23:32:21 +0700

    upload user photo - v1

--- src/server/api/controllers/UsersController.js
+++ src/server/api/controllers/UsersController.js
@@ -1,4 +1,14 @@
 const UserModel = require('./../models/UserModel');
+const catchError = require('./../../utils/catchError');
 const factory = require('./../../helpers/factory');
+
+exports.updateMe = catchError(async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
+
+  res.status(200).json({
+    message: 'ok'
+  });
+});
 
 exports.createUser = factory.createOne(UserModel);
--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const multer = require('multer');
 const AuthController = require('./../api/controllers/AuthController');
-const UsersController = require('./../api/controllers/UsersController');
+const UserController = require('./../api/controllers/UsersController');
+
+const upload = multer({ dest: 'src/assets/img/users' });
 
 const router = express.Router();
 
@@ -8,5 +11,6 @@ router.post('/signup', AuthController.signup);
 router.post('/login', AuthController.login);
 router.get('/logout', AuthController.protect, AuthController.logout);
 router.get('/activate/:token', AuthController.activateAccount);
+router.patch('/updateMe', upload.single('photo'), UserController.updateMe);
 
 module.exports = router;
